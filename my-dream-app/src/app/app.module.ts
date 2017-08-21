6b86b0ea2dced25c4a3e03f9f17a5addb4e9ca66
mgzik <mgzik@ospartner.pl>
2017-08-21 18:48:43 +0200

    added some componenents

--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
+/* import własnego komponentu. bez .ts na końcu */
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
+/* appmodule przechowuje wszystkie moduły aplikacji i zawiera info o wszystkich funkcjonalnościach. nie ma skanowania komponentów w systemie. trzeba wszystko ręcznie zadeklarować */
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-dream-app/src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	/* definiuje nazwę komponentu w HTML */
+	selector: 'app-server',
+	/* adres do componentu */
+	templateUrl: './server.component.html'
+})
+export class ServerComponent {
+	
+
+	
+
+
+}
