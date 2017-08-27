cfb672e2191cdeb5700578d801a0c73e72bd4b07
mgzik <mgzik@ospartner.pl>
2017-08-27 18:07:29 +0200

    styling, components filling

--- shopping-receipe/src/app/app.module.ts
+++ shopping-receipe/src/app/app.module.ts
@@ -6,25 +6,25 @@ import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- shopping-receipe/src/app/header/header.component.ts
+++ shopping-receipe/src/app/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
- 
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-header',
-	templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-	
-	constructor(argument) {
-		// code...
-	}
+export class HeaderComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
