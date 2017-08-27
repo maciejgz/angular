e42dad105573d8a3a7c304f36aa9f0f04d075fff
mgzik <mgzik@ospartner.pl>
2017-08-27 19:19:32 +0200

    data listing, model added

--- shopping-receipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-receipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe("Pizza", "kapriczioza", "http://www.zajadam.pl/wp-content/uploads/2014/08/pizza-z-pomidorami-4-469x313.jpg"),
+		new Recipe("Burger", "just burger", "http://www.seriouseats.com/recipes/assets_c/2015/07/20150728-homemade-whopper-food-lab-35-thumb-1500xauto-425129.jpg"),
+	];
 
   constructor() { }
 
