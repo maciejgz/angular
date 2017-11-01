2c2c70e1153cef04e9e17cd51c1cac3a479957a4
mgzik <mgzik@ospartner.pl>
2017-11-01 19:11:06 +0100

    attribute directives

---
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef , OnInit} from '@angular/core';
+
+	//własna dyrektywa attribute zmieniająca kolor background na elemencie
+
+
+//dodanie selectora dyrektywy w bracketach pozwala na dostęp do niej po tej nazwie niezależnie skąd tego używamy
+@Directive({
+	selector: '[appBasicHighlight]'
+})
+export class BasicHighlightDirective implements OnInit {
+
+	//!!!dodanie private przed parametrem konstruktowa pozwala od razu na utworzenie obiektu dostępnego w tej klasie
+	constructor(private elementRef: ElementRef) {
+	}
+
+
+	ngOnInit(){
+		this.elementRef.nativeElement.style.backgroundColor = 'green';
+
+	}
+
+}
