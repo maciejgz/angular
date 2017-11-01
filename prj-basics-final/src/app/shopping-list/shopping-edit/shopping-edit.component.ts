6f5f313b0435ddba49f03f95f8b708d517344147
mgzik <mgzik@ospartner.pl>
2017-11-01 09:47:55 +0100

    passing data between components final

--- prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,ViewChild, ElementRef,EventEmitter } from '@angular/core';
 
+import { Ingredient } from '../../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+	@Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+	@ViewChild('nameInput') ingredientNameInput : ElementRef;
+	@ViewChild('amountInput') ingredientAmountInput : ElementRef;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+onAddIngredient() {
+	console.log(this.ingredientNameInput.nativeElement.value);
+	const newIngredient = new Ingredient(this.ingredientNameInput.nativeElement.value, this.ingredientAmountInput.nativeElement.value)
+	this.ingredientAdded.emit(newIngredient);
+}
 
 }
