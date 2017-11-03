24a479930829b69a0c3bd0f91d9967aafd90c606
maciejgz <mgzik@ospartner.pl>
2017-11-03 19:04:17 +0100

    service injection

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -5,10 +5,18 @@ import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+//tutaj odbywa się injection serwisu. w konstruktorze
+// angular odpowiada za tworzenie obiektów
+// do wstrzykiwania potrzebujemy providera
+constructor(private loggingService: LoggingService) {
+
+}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
@@ -17,7 +25,9 @@ export class NewAccountComponent {
     });
 
     //utworzenie obiektu serwisu to jest bardzo złe podejście
-    const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+    //const service = new LoggingService();
+
+    //podejście z wstrzykniętym obiektem
+    this.loggingService.logStatusChange(accountStatus);
    }
 }
